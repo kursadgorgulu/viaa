3ffc384cf91afcf822d2f983907aa3c1098ef717
kursadgorgulu <kurreeeh@gmail.com>
2020-04-16 14:36:42 +0200

    Delete file

--- scripts/normalization scripts/normalize_files_cgn.sh
+++
@@ -1,6 +0,0 @@
-## This script performs built-in rules and regular expression to normalize all text files in the current folder
-
-for f in *.txt; do
-    benchmarkstt-tools normalization --inputfile "$f" --outputfile "reference_$f" --lowercase --regex "[,.\?!:\"\'\´\`\-\–\−\–\—;]|(xxx)|(ggg)|(\*v)|(\*d)|(\*z)|(\*n)|(\*t)|(\*a)|(\*u)|(\*x)" ""
-    echo "Processing $f"
-done
