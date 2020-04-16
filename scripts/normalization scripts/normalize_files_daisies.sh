4f4a9179f9e9e4ff4576bd749ce2dfb3d682d162
kursadgorgulu <kurreeeh@gmail.com>
2020-04-16 14:37:52 +0200

    Delete file

--- scripts/normalization scripts/normalize_files_daisies.sh
+++
@@ -1,6 +0,0 @@
-## This script performs built-in rules and regular expressions to normalize all text files in the current folder
-
-for f in *.txt; do
-    benchmarkstt-tools normalization --inputfile "$f" --outputfile "reference_$f" --lowercase --regex "[,.\?!:\"\'\´\`\-\–\−\–\—;]" ""
-    echo "Processing $f"
-done
