466745668eef8fd38a909a9b1edb437efa1c45f3
kursadgorgulu <kurreeeh@gmail.com>
2020-04-16 14:38:21 +0200

    Delete file

--- scripts/normalization scripts/normalize_files_sheffield.sh
+++
@@ -1,7 +0,0 @@
-## This script performs built-in rules and regular expressions to normalize all text files in current folder
-## Remove numbers 1, 2, 3 and 4 because transcription text containts chapter sequence numbers which are not pronounced by speaker
-
-for f in *.txt; do
-    benchmarkstt-tools normalization --inputfile "$f" --outputfile "reference_$f" --lowercase --regex "[,.\?!:\"\'\´\`\-\–\−\–\—;1234]" ""
-    echo "Processing $f"
-done
