44e152afc5838d0cd58f71225d3ad19a6c2f2e54
kursadgorgulu <kurreeeh@gmail.com>
2020-05-02 17:04:30 +0200

    Add files via upload

---
+++ scripts/normalize_reference_files.sh
@@ -0,0 +1,9 @@
+## This script performs built-in rules and regular expression to normalize all text files in the current folder. 
+
+## Regular expression for normalization
+regexppr="[,.\?!:\"\'\´\`\-\–\−\–\—;*‘’]|(xxx)|(ggg)|(\*v)|(\*d)|(\*z)|(\*n)|(\*t)|(\*a)|(\*u)|(\*x)"
+   
+for f in *.txt; do
+    benchmarkstt-tools normalization --inputfile "$f" --outputfile "reference_$f" --lowercase --regex "$regexppr" ""
+    echo "Processing $f"
+done
