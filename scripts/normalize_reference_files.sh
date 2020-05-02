c0f9af640ca05c25c7e2bf9cd33055c4337ae382
kursadgorgulu <kurreeeh@gmail.com>
2020-05-02 18:27:26 +0200

    Update file

--- scripts/normalize_reference_files.sh
+++ scripts/normalize_reference_files.sh
@@ -1,7 +1,7 @@
 ## This script performs built-in rules and regular expression to normalize all text files in the current folder. 
 
 ## Regular expression for normalization
-regexppr="[,.\?!:\"\'\´\`\-\–\−\–\—;*‘’]|(xxx)|(ggg)|(\*v)|(\*d)|(\*z)|(\*n)|(\*t)|(\*a)|(\*u)|(\*x)"
+regexppr="[,.\?!:\"\'\´\`\-\–\−\–\—;*‘’()]|(xxx)|(ggg)|(\*v)|(\*d)|(\*z)|(\*n)|(\*t)|(\*a)|(\*u)|(\*x)"
    
 for f in *.txt; do
     benchmarkstt-tools normalization --inputfile "$f" --outputfile "reference_$f" --lowercase --regex "$regexppr" ""
